42d8c49067333f152584cd33f0b3e271a73f3b71
Christian-Brandtner <christianbrandtner32@gmail.com>
2024-06-13 22:33:21 -0700

    add proper working detection

--- drill.py
+++ drill.py
@@ -1,20 +1,17 @@
 import time
 import RPi.GPIO as GPIO
-
-GPIO.setmode (GPIO.BCM)
-
+GPIO.setmode(GPIO.BCM)
 tach_pin = 17
-
 GPIO.setup(tach_pin, GPIO.IN)
-
-avg_iterate = 50
-
+avg_iterate = 20
 time_between = time.perf_counter()
 prev_time = time.perf_counter()
 prev_state = GPIO.HIGH
 avg_array = [0.0000] * avg_iterate
 time_array = [0.0000] * avg_iterate
 counter = 0
+one_count = 0
+time_before = time.perf_counter()
 
 def calc_engine_RPM(seconds):
     rpm = 60*(1/seconds)
@@ -23,19 +20,26 @@ def calc_engine_RPM(seconds):
 try:
     while True:
         current_state = GPIO.input(tach_pin)
-        
         current_time = time.perf_counter()
-        
-        if prev_state == GPIO.HIGH and current_state == GPIO.LOW and (current_time - prev_time) >= (time_array[counter] * 0.2):
+        time_between = current_time - prev_time
+        if prev_state == GPIO.HIGH and current_state == GPIO.LOW and (current_time - prev_time) >= 0.005 and one_count == 0:
             prev_time = current_time
-            time_between = current_time - prev_time
             time_array[counter] = time_between
-        avg_array[counter] = calc_engine_RPM(time_between)
-        counter = (counter + 1) % avg_iterate
-        
-        avg = sum(avg_array) / avg_iterate
-        rounded_avg = round(avg)
+            prev_time = current_time
+            one_count += 1
+            prev_state = 0
+            print(rounded_avg)
+            print("\n")
+        if current_state == GPIO.HIGH:
+            one_count = 0
+            prev_state = 1
+            avg_array[counter] = calc_engine_RPM(time_between)
+            counter = (counter + 1) % avg_iterate
+            avg = sum(avg_array) / avg_iterate
+            rounded_avg = round(avg)
+            if current_time - time_before >= 0.0161616:
+                time_before = current_time
 except KeyboardInterrupt:
-        print("\n Exiting program")
+    print("\n Exiting program")
 finally:
     GPIO.cleanup()
