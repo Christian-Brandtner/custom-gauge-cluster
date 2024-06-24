d987c9793c6d4e47460d3631a770717dff0ce149
Christian-Brandtner <christianbrandtner32@gmail.com>
2024-06-23 17:34:57 -0700

    add basic speed levels

--- src/gauges/speedometer.py
+++ src/gauges/speedometer.py
@@ -7,48 +7,55 @@ speed_pin = 17
 # UPDATE TO USE CLASS
 # ALSO, REMOVE ALL COMMENTS, ADD SPEED LEVEL FUNCTIONALITY, AND EXPERIMENT WITH bounce_time, USE EXPONENTIAL SMOOTHING
 
-# SPEED STEP
-    # if(all magnet pulse > 7200 (720*10magnets aka 15mph)){
-    #   if pulse count == 2, 4, 6, 8, 10 {calculate speed based on rpm/5} 
-    #}
-    # if(all pulse > 21000) {calculate based on rpm/10}
-    # else(calc based on all pulse)
+# CREATE FUNCTION TO CALCULATE TIME_BETWEEN AT < 15, < 30 AND > 30 MPH. DONE AT STARTUP, BASED ON CONFIGS LIKE TIRE SIZE, DRIVE RATIO ETC. 
 
 
-# Set up the pin with internal pull-up resistor
 hall_sensor = DigitalInputDevice(speed_pin, pull_up=True, bounce_time=0.001)
 
 AVERAGE_ITERATE = 20
+MAGNET_COUNT = 10
 
-time_between = time.perf_counter()
-prev_time = time.perf_counter()
+time_between = time.perf_avg_counter()
+prev_time = time.perf_avg_counter()
 
 avg_array = [0.0000] * AVERAGE_ITERATE
-time_array = [0.0000] * AVERAGE_ITERATE
+time_array = [0.0000] * (MAGNET_COUNT-1)
 
-counter = 0
+time_counter = 0
+avg_counter = 0
 edge_check = 0
-prev_state = 1  # Initial value for prev_state
+prev_state = 1
+
+def calc_seconds(seconds):
+    if seconds > 0.00835:
+        return seconds
+    elif seconds < 0.00835 and seconds > 0.004147:
+        seconds = sum(time_array[0:9:2]) / 5
+    elif seconds < 0.004147:
+        seconds = time_array[0]
+    return seconds
 
 def calc_speed(seconds):
-    if seconds == 0 or seconds > 2:
+    if seconds == 0 or seconds > 1.2:
         return 0
     else:
-        rpm = 60 / seconds
-        return rpm
+        speed = 60 / seconds
+        return speed
 
 def smooth_speed():
-    global counter, avg_array, time_between
-    avg_array[counter] = calc_speed(time_between)
+    global avg_counter, avg_array, time_between
+    avg_array[avg_counter] = calc_speed(calc_seconds(time_array))
     avg = sum(avg_array) / AVERAGE_ITERATE
     rounded_avg = round(avg)
     return rounded_avg
 
 def hall_detect():
-    global prev_time, counter, time_between
-    time_between = time.perf_counter() - prev_time
-    prev_time = time.perf_counter()
-    counter = (counter + 1) % AVERAGE_ITERATE
+    global prev_time, avg_counter, time_between
+    time_between = time.perf_avg_counter() - prev_time
+    prev_time = time.perf_avg_counter()
+    avg_counter = (avg_counter + 1) % AVERAGE_ITERATE
+    time_counter = (time_counter + 1) % MAGNET_COUNT
+    time_array[time_counter] = time_between
     
 hall_sensor.when_activated = hall_detect
 
@@ -59,4 +66,4 @@ try:
 except KeyboardInterrupt:
     print("\nExiting program")
 finally:
-    pass  # gpiozero handles cleanup automatically
+    pass
