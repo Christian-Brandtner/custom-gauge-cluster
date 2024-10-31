cae2f10abe82489f56aa596d66bb24d5acc35ef7
Christian-Brandtner <christianbrandtner32@gmail.com>
2024-10-30 20:11:08 -0700

    adjustments

--- src/display.py
+++ src/display.py
@@ -12,8 +12,10 @@ SCREEN_HEIGHT = get_display_config().get('RESOLUTION_Y')
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-gauge_bg = pygame.image.load('assets/images/gauges/speedometer/speed_gauge_bg.png')
-gauge_fg = pygame.image.load('assets/images/gauges/speedometer/speed_gauge.png')
+gauge_bg = pygame.image.load(
+    'assets/images/gauges/speedometer/speed_gauge_bg.png')
+gauge_fg = pygame.image.load(
+    'assets/images/gauges/speedometer/speed_gauge.png')
 
 gauge_bg_rect = gauge_bg.get_rect()
 gauge_fg_rect = gauge_fg.get_rect()
@@ -24,25 +26,34 @@ angle = 0
 MAX_SPEED = 100
 MAX_ANGLE = 180
 
+prev_time = 0.0
+current_time = 0.0
+between_time = 0.0
+
+
 fake_speedometer = speed.FakeSpeedometer()
 try:
     while True:
-        screen.fill((0, 0, 0))
-        
-        rotated_gauge_bg = pygame.transform.rotate(gauge_bg, angle)
-        rotated_gauge_bg_rect = rotated_gauge_bg.get_rect(center=gauge_fg_rect.center)
-        speed_text = font.render(f"{fake_speedometer.calc_speed()}", True, (235, 235, 235))
-        speed_text = font.render(f"40", True, (235, 235, 235))
-        #shaft_RPM_text = font.render(f"Shaft RPM: {int(shaft_RPM)}", True, (235, 235, 235))
-        
-        
-        screen.blit(rotated_gauge_bg, rotated_gauge_bg_rect)
-        screen.blit(gauge_fg, gauge_fg_rect)
-        screen.blit(speed_text, (gauge_fg_rect.center))
-        #screen.blit(shaft_RPM_text, (SCREEN_WIDTH // 8, gauge_fg_rect.centery))
-        angle = -((fake_speedometer.calc_speed() / MAX_SPEED) * MAX_ANGLE)
-        fake_speedometer.hall_detect()
-        
-        pygame.display.flip()
+
+        current_time = float(time.perf_counter())
+        speed_text = font.render(
+            f"{round(fake_speedometer.calc_speed(), 1)}", True, (235, 235, 235))
+        angle = -((round(fake_speedometer.calc_speed(), 4) / MAX_SPEED) * MAX_ANGLE)
+
+        if float(current_time - prev_time) >= float(1/40):
+            prev_time = current_time
+            screen.fill((0, 0, 0))
+            fake_speedometer.hall_detect()
+            rotated_gauge_bg = pygame.transform.rotate(gauge_bg, angle)
+            rotated_gauge_bg_rect = rotated_gauge_bg.get_rect(
+                center=gauge_fg_rect.center)
+
+            screen.blit(rotated_gauge_bg, rotated_gauge_bg_rect)
+            screen.blit(gauge_fg, gauge_fg_rect)
+            screen.blit(speed_text, (gauge_fg_rect.center))
+            #screen.blit(shaft_RPM_text, (SCREEN_WIDTH // 8, gauge_fg_rect.centery))
+
+            pygame.display.flip()
+
 except KeyboardInterrupt:
     print("exit")
