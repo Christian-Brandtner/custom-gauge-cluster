fbb942eb1a3c85c2f111215ef341c9e852b58924
Christian-Brandtner <christianbrandtner32@gmail.com>
2024-11-05 19:32:05 -0800

    fix main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import time
-#import gauges.speedometer
+import gauges.speedometer
 # import gauges.fake_speed
 import display
 
 
 if __name__ == "__main__":
-    #speedometer = gauges.speedometer.Speedometer(speed_pin=17)
+    speedometer = gauges.speedometer.Speedometer(speed_pin=17)
     # speedometer = gauges.fake_speed.FakeSpeedometer()
     # speedometer.run()
     display
