cfed95641a2619f8128dab0afc92de4b1e584aa9
Christian-Brandtner <christianbrandtner32@gmail.com>
2024-11-08 16:07:56 -0800

    testing

--- src/gauges/speedometer.py
+++ src/gauges/speedometer.py
@@ -66,4 +66,5 @@ class Speedometer:
         self.time_array[self.time_counter] = self.time_between
 
     def run(self):
-        self.hall_sensor.when_activated = self.hall_detect()
+        self.hall_sensor.when_activated = self.hall_detect
+        self.hall_sensor.when_activated = print("active")
