140c2f4e40af57fec263af1566b9253baaba6da1
Christian-Brandtner <christianbrandtner32@gmail.com>
2024-11-13 14:46:39 -0800

    fix 0mph and division

--- src/gauges/speedometer.py
+++ src/gauges/speedometer.py
@@ -15,7 +15,7 @@ class Speedometer:
         self.SENSOR_DIVIDERS = {
             0: 1,
             1: 5, # CALCULATED BASED OFF MAGNET_COUNT
-            3: 10
+            2: 10
         }
         self.time_between = time.perf_counter()
         self.prev_time = time.perf_counter()
@@ -27,24 +27,6 @@ class Speedometer:
         self.calc_zero = True
         self.tire_circumference = math.pi * self.TIRE_DIAMETER
 
-
-    def calc_speed_time(self):
-        speed_step = [15, 30]
-        speed_time = [0.0000] * len(speed_step)
-        for index, speed in enumerate(speed_step):
-            speed_time[index] = 6 / (self.DRIVE_RATIO * (
-                (self.INCHES_PER_MIN_TO_MPH * speed) / (math.pi * self.TIRE_DIAMETER)))
-        return speed_time
-
-    def calc_seconds(self, seconds):
-        if seconds > self.calc_speed_time()[0]:
-            return seconds
-        elif seconds < self.calc_speed_time()[1] and seconds >= self.calc_speed_time()[0]:
-            seconds = sum(self.time_array[0:9:2]) / (self.MAGNET_COUNT[0] // 2)
-        elif seconds < self.calc_speed_time()[1]:
-            seconds = self.time_array[0]
-        return seconds
-
     def calc_shaft_rpm(self, seconds, calc_zero):
         if calc_zero:
             return 0
@@ -54,15 +36,14 @@ class Speedometer:
 
     def calc_speed(self):
         if self.calc_if_zero():
-            speed = 0
-        else:
-            wheel_rpm = self.smooth_rpm() / self.DRIVE_RATIO
-            speed = (self.tire_circumference * wheel_rpm) / self.INCHES_PER_MIN_TO_MPH
+            self.time_array[self.time_counter] = 0
+        wheel_rpm = self.smooth_rpm() / self.DRIVE_RATIO
+        speed = (self.tire_circumference * wheel_rpm) / self.INCHES_PER_MIN_TO_MPH
         return speed
 
     def smooth_rpm(self):
         self.avg_array[self.avg_counter] = self.calc_shaft_rpm(
-            self.calc_seconds(self.time_array[self.time_counter]), self.calc_if_zero())
+            self.time_array[self.time_counter], self.calc_if_zero())
         avg = sum(self.avg_array) / self.AVERAGE_ITERATE
         rounded_avg = round(avg)
         return rounded_avg
@@ -91,3 +72,19 @@ class Speedometer:
     #     self.hall_sensor.when_activated = self.hall_detect
     #     self.hall_sensor.when_activated = print("active")
         
+# def calc_speed_time(self):
+    #     speed_step = [15, 30]
+    #     speed_time = [0.0000] * len(speed_step)
+    #     for index, speed in enumerate(speed_step):
+    #         speed_time[index] = 6 / (self.DRIVE_RATIO * (
+    #             (self.INCHES_PER_MIN_TO_MPH * speed) / (math.pi * self.TIRE_DIAMETER)))
+    #     return speed_time
+
+    # def calc_seconds(self, seconds):
+    #     if seconds > self.calc_speed_time()[0]:
+    #         return seconds
+    #     elif seconds < self.calc_speed_time()[1] and seconds >= self.calc_speed_time()[0]:
+    #         seconds = sum(self.time_array[0:9:2]) / (self.MAGNET_COUNT[0] // 2)
+    #     elif seconds < self.calc_speed_time()[1]:
+    #         seconds = self.time_array[0]
+    #     return seconds
